9e32dc0ae3f8f39c4ee2ffed8b02469562376592
Franco Nicolás Placidi <79880040+francoplacidi@users.noreply.github.com>
2021-09-23 18:29:12 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-console.log("Aplicacion Calculadora");
+console.log("Calculator App");
 
-function sumar(){
-	const forma = document.getElementById('forma');	
-	let operandoA = forma['operandoA'];
-	let operandoB = forma['operandoB'];
-	let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-		if(isNaN(resultado)){
-			resultado = 'La operacion no incluye numeros';
+function sum(){
+	const shape = document.getElementById('shape');	
+	let operatingA = shape['operatingA'];
+	let operatingB = shape['operatingB'];
+	let result = parseInt(operatingA.value) + parseInt(operatingB.value);
+		if(isNaN(result)){
+			result = 'The operation does not include numbers';
 		}
-	document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
-	console.log(`Resultado: ${resultado}`);
+	document.getElementById('result').innerHTML = `Result: ${result}`;
+	console.log(`Result: ${result}`);
 }
